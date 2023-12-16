124c30e7229b77404a249e407a885fb77cfddb64
AHarshitha04 <harshitha.a@egradtutor.in>
2023-12-16 14:35:33 +0530

    hh

--- src/Pages/General_intructions_page/General_intructions_page.jsx
+++ src/Pages/General_intructions_page/General_intructions_page.jsx
@@ -1,3 +1,307 @@
+// import React, { useEffect, useRef, useState } from "react";
+// import {
+//   General_intructions_page_content,
+//   Navbar,
+// } from "../../Data/Introduction_page_DATA";
+// import { Link, useNavigate, useParams } from "react-router-dom";
+// import axios from "axios";
+// import { AiOutlineArrowRight } from "react-icons/ai";
+// import Paper from "../Paper/Paper";
+
+// // import Paper1 from '../Paper/Paper1'
+// // import Paper1 from '../Paper/Paper1';
+
+// const General_intructions_page = () => {
+//   return (
+//     <>
+//       <General_intructions_page_header />
+//       <General_intructions_page_container seconds={600} />
+//     </>
+//   );
+// };
+
+// export default General_intructions_page;
+
+// export const General_intructions_page_header = () => {
+//   return (
+//     <>
+//       {Navbar.map((nav, index) => {
+//         return (
+//           <div className="Quiz_General_header" key={index}>
+//             <h1>{nav.Q_page_title}</h1>
+//             <div className="Q_title">{/* <p>{nav.time_limt}</p> */}</div>
+//           </div>
+//         );
+//       })}
+//     </>
+//   );
+// };
+
+// export const General_intructions_page_container = ({}) => {
+//   // const [subjectData, setSubjectData] = useState([]);
+
+//   // useEffect(() => {
+//   //   const fetchSubjects = async () => {
+//   //     try {
+//   //       const response = await fetch(
+//   //         `http://localhost:4009/subjectData`
+//   //       );
+//   //       const data = await response.json();
+//   //       setSubjectData(data);
+//   //     } catch (error) {
+//   //       console.error(error);
+//   //     }
+//   //   };
+
+//   //   fetchSubjects();
+//   // }, [testCreationTableId]);
+
+//   // const [countdown, setCountdown] = useState(seconds);
+//   // const timerId = useRef();
+
+//   // useEffect(() => {
+//   //   if (countdown <= 0) {
+//   //     clearInterval(timerId.current);
+//   //     alert("End");
+//   //   }
+//   // }, [countdown]);
+//   // const navigate = useNavigate();
+//   // const startCountdown = () => {
+//   //   timerId.current = setInterval(() => {
+//   //     setCountdown((prev) => prev - 1);
+//   //   }, 1000);
+//   //   navigate("/Paper1");
+//   // };
+
+//   const [isChecked, setIsChecked] = useState(false);
+
+//   const handleCheckboxChange = (e) => {
+//     setIsChecked(e.target.checked);
+//   };
+
+//   const [instructionsData, setInstructionsData] = useState([]);
+//   const { examid, testCreationTableId } = useParams();
+//   // console.log("examid:", examid);
+
+//   useEffect(() => {
+//     const fetchInstructions = async () => {
+//       try {
+//         const response = await fetch(
+//           `http://localhost:4009/fetchinstructions/${testCreationTableId}/`
+//         );
+//         const data = await response.json();
+//         setInstructionsData(data);
+//         // setSubjectData(instructionsData);
+//         // console.log(instructionsData)
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchInstructions();
+//   }, [testCreationTableId]);
+//   // const [testCreationTableId] = useState([]);
+//   // const fetchInstructions = async (testCreationTableId) => {
+//   //   try {
+//   //     const response = await fetch(
+//   //       `http://localhost:4009/fetchinstructions/${testCreationTableId}/`
+//   //     );
+//   //     const data = await response.json();
+
+//   //     if (Array.isArray(data)) {
+//   //       setInstructionsData(data);
+//   //     } else {
+//   //       console.error("Data is not an array:", data);
+//   //     }
+//   //   } catch (error) {
+//   //     console.error(error);
+//   //   }
+//   //   fetchInstructions();
+//   // };
+
+  
+
+//   // useEffect(() => {
+//   //   const fetchSubjects = async () => {
+//   //     try {
+//   //       const response = await fetch(
+//   //         `http://localhost:4009/subjectData/${subjectId}`
+//   //       );
+//   //       const data = await response.json();
+//   //       setSubjectData(data);
+//   //       console.log(SubjectData);
+//   //     } catch (error) {
+//   //       console.error(error);
+//   //     }
+//   //   };
+//   //   fetchSubjects();
+//   // }, [subjectId]);
+
+//   // const [subjectId, setSubjectId] = useState([]);
+//   // const handlePaperData = async (typeOfTestId) => {
+//   //   console.log("working");
+//   //   try {
+//   //     // Fetch tests based on typeOfTestId
+//   //     const response = await fetch(
+//   //       `http://localhost:4009/feachingtestbytype/${typeOfTestId}`
+//   //     );
+//   //     const testData = await response.json();
+//   //     setSubjectId(testData);
+//   //   } catch (error) {
+//   //     console.error(error);
+//   //   }
+//   // };
+
+//   //   const [questionData, setQuestionData] = useState([]);
+//   // const [ setSubjectId] = useState([]);
+//   //   useEffect(() => {
+//   //     const fetchQuestionDataBySubjectId = async () => {
+//   //       try {
+//   //         const response = await fetch(
+//   //           `http://localhost:4009/getPaperData/${testCreationTableId}/${subjectId}`
+//   //         );
+//   //         const data = await response.json();
+
+//   //         // Check if the data has both 'questions' and 'options' properties
+//   //         if (data && data.questions && data.options) {
+//   //           // Assuming both questions and options are arrays, you might want to merge them
+//   //           const mergedData = data.questions.map((question, index) => ({
+//   //             ...question,
+//   //             options: data.options.slice(index * 4, (index + 1) * 4), // Assuming 4 options per question
+//   //           }));
+
+//   //           setQuestionData(mergedData);
+//   //         } else {
+//   //           const testData = await response.json();
+//   //           console.error("API response does not have expected structure:", data);
+//   //           setSubjectId(testData);
+//   //         }
+//   //       } catch (error) {
+//   //         console.error(error);
+//   //       }
+//   //     };
+
+//   //     fetchQuestionDataBySubjectId();
+//   //   }, [testCreationTableId]);
+
+//   // const [instructionsData, setInstructionsData] = useState([]);
+//   // const { examid } = useParams();
+
+//   // const fetchInstructions = async (testCreationTableId) => {
+//   //   try {
+//   //     const response = await fetch(
+//   //       `http://localhost:4009/fetchinstructions/${testCreationTableId}`
+//   //     );
+//   //     const data = await response.json();
+
+//   //     if (Array.isArray(data)) {
+//   //       setInstructionsData(data);
+//   //     } else {
+//   //       console.error("Data is not an array:", data);
+//   //     }
+//   //   } catch (error) {
+//   //     console.error(error);
+//   //   }
+//   // };
+
+//   // useEffect(() => {
+//   //   fetchInstructions(examid);
+//   // }, [examid]);
+
+//   return (
+//     <>
+//       <div>
+//   <h2>General Instructions</h2>
+//   <ul>
+//         {instructionsData.map((instruction, index) => (
+//             <React.Fragment key={instruction.id}>
+//               {index === 0 && <li>{instruction.instructionHeading}</li>}
+//               <li>{instruction.points}</li>
+//             </React.Fragment>
+//           ))}
+//         </ul>
+//   {/* {instructionsData && instructionsData.length > 0 && (
+//     <ul>
+//       {instructionsData.map((instructionsPoints, index) => (
+//         <li key={instructionsPoints.id}>
+//           {index === 0 && (
+//             <strong>{instructionsPoints.instructionHeading}</strong>
+//           )}
+//           <span>{instructionsPoints.points}</span>
+//         </li>
+//       ))}
+//     </ul>
+
+
+//   )} */}
+// {/* {instructionsData.map((instructionsData,id)=>{
+//   return(
+//     <div key={id}>
+
+// <strong>{instructionsData.points}</strong>
+//     </div>
+//   )
+// }) */}
+// {/* } */}
+
+// </div>
+
+
+//       {/* <div>
+//         <h2>General Instructions</h2>
+//         {instructionsData && instructionsData.length > 0 && (
+//           <ul>
+//             {instructionsData.map((instructionsPoints, index) => (
+//               <li key={instructionsPoints.id}>
+//                 {index === 0 && (
+//                   <strong>{instructionsPoints.instructionHeading}</strong>
+//                 )}
+//                 <li>{instructionsPoints.points}</li>
+//               </li>
+//             ))}
+//           </ul>
+//         )}
+//       </div> */}
+
+//       <div>{/* Your checkbox and agreement code here */}</div>
+
+//       <div>
+//         <div className="gn_checkbox">
+//           <input
+//             type="checkbox"
+//             onChange={handleCheckboxChange}
+//             className="checkbox"
+//           />
+//           <p>
+//             {" "}
+//             I agree to these <b> Terms and Conditions.</b>
+//           </p>
+//         </div>
+//       </div>
+//       <div className="gn_next_btn_container">
+//         {isChecked ? (
+//           <div>
+//             {" "}
+//             {instructionsData.length > 0 && (
+//               <Link to={`/subjects`} className="gn_next_btn">
+//                 I am ready to begin <AiOutlineArrowRight />
+//               </Link>
+//             )}
+//           </div>
+//         ) : (
+//           <div>
+//             <span className="disabled-link gn_next_btn_bull ">
+//               I am ready to begin <AiOutlineArrowRight />
+//             </span>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+
+// };
+
+
 import React, { useEffect, useRef, useState } from "react";
 import {
   General_intructions_page_content,
@@ -165,6 +469,74 @@ export const General_intructions_page_container = ({ seconds }) => {
   //     fetchQuestionDataBySubjectId();
   //   }, [testCreationTableId]);
 
+  // const [courseCreationId]=useParams('')
+
+  // const [minsubjectid, setminsubjectid] = useState('');
+  const [minsubjectid, setminsubjectid] = useState("");
+  useEffect(() => {
+    const fetchSubjects = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:4009/subjectData2/${testCreationTableId}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        // Corrected line: Set the state using the retrieved data
+        setSubjectData(data);
+        console.log(data);
+
+        if (data && data.length > 0) {
+          // Find the minimum value of subjectId in the array
+          const minSubjectId = Math.min(...data.map((item) => item.subjectId));
+
+          // Log the minimum value to the console
+          console.log("Minimum subjectId:", minSubjectId);
+          
+          setminsubjectid(minSubjectId);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchSubjects();
+  }, [subjectId]);
+  // useEffect(() => {
+  //   const fetchSubjects = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `http://localhost:4009/subjectData/${courseCreationId}`
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+
+  //       const data = await response.json();
+  //       setSubjectData(data);
+
+  //       if (data && data.length > 0) {
+  //         // Find the minimum value of subjectId in the array
+  //         const minSubjectId = Math.min(...data.map((item) => item.subjectId));
+
+  //         // Log the minimum value to the console
+  //         console.log("Minimum subjectId:", minSubjectId);
+
+  //         setminsubjectid(minSubjectId);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchSubjects();
+  // }, [subjectId]);
+
   return (
     <>
       <div>
@@ -237,13 +609,18 @@ export const General_intructions_page_container = ({ seconds }) => {
           // </Link>
           // ))}</div>
           //
+           //-------------------------------WORKING CODE-----------------------------------
+          //   <Link
+          //   // to={`/subjects/${testCreationTableId}/${minsubjectid}`}
+          //   to={`/subjects/${testCreationTableId}/1`}
 
-            <Link
-            to={`/subjects/${testCreationTableId}/${subjectId}`}
-            className="gn_next_btn"
-            >
-            I am ready to begin <AiOutlineArrowRight />
-          </Link>
+          //   className="gn_next_btn"
+
+          //   // to={`/General_intructions_page/${test.testCreationTableId}/${minsubjectid}`}
+          //   >
+          //   I am ready to begin <AiOutlineArrowRight />
+          // </Link>
+            //-------------------------------WORKING CODE-----------------------------------
           // <Link
           //   to={`/subjects/${testCreationTableId}/${
           //     subjectId || leastSubjectId
@@ -252,6 +629,16 @@ export const General_intructions_page_container = ({ seconds }) => {
           // >
           //   I am ready to begin <AiOutlineArrowRight />
           // </Link>
+          <Link
+            // to={`/subjects/${testCreationTableId}/${minsubjectid}`}
+            to={`/getPaperData/${testCreationTableId}/${minsubjectid}`}
+
+            className="gn_next_btn"
+
+            // to={`/General_intructions_page/${test.testCreationTableId}/${minsubjectid}`}
+            >
+            I am ready to begin <AiOutlineArrowRight />
+          </Link>
         ) : (
           <div>
             <span className="disabled-link gn_next_btn_bull ">
